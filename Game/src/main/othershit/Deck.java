a1e75b17ab2dc88b2f5fb24622eb6ce0814ae38e
Martin <mjch17@student.aau.dk>
2018-09-21 12:07:00 +0200

    Added a few methods to the deck.

--- Game/src/main/othershit/Deck.java
+++ Game/src/main/othershit/Deck.java
@@ -1,4 +1,31 @@
 package main.othershit;
 
+import java.util.LinkedList;
+import java.util.Random;
+
 public class Deck {
+    private LinkedList<Card> cards;
+
+    public void shuffle(){
+        Random srand = new Random();
+        for(int i = 0; i < this.cards.size() * 2; ++i ) {
+            Card storage = this.cards.get(i);
+            this.cards.remove(i);
+            this.cards.add(srand.nextInt(this.cards.size() - 1), storage);
+        }
+    }
+
+    public Card drawCard(){
+        Card card = this.cards.getFirst();
+        this.cards.remove(this.cards.getFirst());
+        return card;
+    }
+
+    public Card seeCard(int entry){
+        return this.cards.get(entry);
+    }
+
+    public Deck(LinkedList<Card> cards) {
+        this.cards = cards;
+    }
 }
